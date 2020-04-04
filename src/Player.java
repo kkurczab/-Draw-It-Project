603fc4f277213c96bd91cb46c0e67ab65e29feae
Karolz98 <44200387+Karolz98@users.noreply.github.com>
2020-04-04 11:52:47 +0200

    Podstawowe połączenie

--- src/Player.java
+++ src/Player.java
@@ -1,2 +1,46 @@
-public class Player {
+import java.io.*;
+import java.net.*;
+
+public class Player {//szukanie adresu ip to bedzie iteracja po wszystkich adresach w podsieci XD, bo serwerem bedzie host ktory pierwszy odpali gre
+    private String ipSerwera = "localhost";//tymczasowo do pierwszych testow///isReachable useful!!!
+    private PolaczenieOdKlienta polaczenieOdKlienta;
+    private int playerID;
+    private int[] pozostaliGracze;
+    /////////////Metody
+
+
+
+
+    //////////////Kontruktor
+    public Player(){
+
+    }
+    public void polaczZSerwerem(){
+        polaczenieOdKlienta = new PolaczenieOdKlienta();
+    }
+    ///////////////////Klasy
+    private class PolaczenieOdKlienta{
+        private Socket socket;
+        private DataInputStream daneIN;
+        private DataOutputStream daneOUT;
+
+        public PolaczenieOdKlienta(){
+            System.out.println("Klient!!!!");
+            try{
+                socket = new Socket(ipSerwera,51724);///InetAddress.getByAddress(new byte[] {(byte)192, (byte)168, (byte)0, (byte)102}),
+                daneIN = new DataInputStream(socket.getInputStream());
+                daneOUT = new DataOutputStream(socket.getOutputStream());
+                playerID = daneIN.readInt();
+                System.out.println("Moj numer gracza to: " + playerID);
+            }catch (IOException e)
+            {
+                System.out.println("Skocket w kontruktorze pol klienta");
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Player player = new Player();
+        player.polaczZSerwerem();
+    }
 }
--- src/Server.java
+++ src/Server.java
@@ -1,2 +1,82 @@
+import java.io.*;
+import java.net.*;
+
 public class Server {
+
+    private ServerSocket socketSerwer;
+    private int liczbaGraczy;
+    private PolaczenieDoKlienta[] gracze;
+    private int maxLiczbaGraczy;
+
+
+   ///////////////////Metody
+    public void przyjeciePolaczenia(){
+        System.out.println("Oczekiwanie na graczy");
+        try {
+            while (liczbaGraczy < maxLiczbaGraczy) {//warunke dodatkowy jesli chcemy zeby wczesniej zakonczyl oczekiwanie na graczy
+                Socket socket = socketSerwer.accept();//accept zwraca socket,(kliencka wersja serwer socketa)
+                liczbaGraczy++;
+                System.out.println("Gracz nr " + liczbaGraczy + " polaczyl sie");
+                PolaczenieDoKlienta polaczenieDoKlienta = new PolaczenieDoKlienta(socket,liczbaGraczy);
+                gracze[liczbaGraczy-1] = polaczenieDoKlienta;
+                Thread t = new Thread(polaczenieDoKlienta);
+                t.start();
+            }
+            System.out.print("Maksymalna liczb garczy osiganieta!!!");
+        }catch(IOException e){
+            System.out.println("przechwycono wyjątek z podlaczania klienta ");
+        }
+    }
+
+    ///////////////////Konstruktor
+    public Server(){
+        maxLiczbaGraczy = 5;
+        gracze = new PolaczenieDoKlienta[maxLiczbaGraczy];
+        System.out.print("Serwer ruszyl!!!");
+        liczbaGraczy = 0;
+        try{
+            socketSerwer = new ServerSocket(51724);
+        }catch(IOException e){
+            System.out.print("przechwycono wyjątek z kontrukcji socketa");
+        }
+    }
+
+    ////Klasy
+
+    private class  PolaczenieDoKlienta implements Runnable{
+        private Socket socket;
+        private DataOutputStream daneOUT;
+        private DataInputStream daneIN;
+        private int playerID;
+
+        public  PolaczenieDoKlienta(Socket s, int id) {
+            socket = s;
+            playerID = id;
+            try {
+                daneIN = new DataInputStream(socket.getInputStream());
+                daneOUT = new DataOutputStream(socket.getOutputStream());
+
+            } catch (IOException e) {
+                System.out.println("Wyjatek z polaczena strona serwera");
+
+            }
+        }
+        public void run(){
+            try{
+                daneOUT.writeInt(playerID);
+                daneOUT.flush();
+                while(true){
+
+                }
+
+            }catch (IOException e){
+
+            }
+        }
+    }
+    /////////////////////////Main
+    public static void main(String[] args) {
+        Server serwer = new Server();
+        serwer.przyjeciePolaczenia();
+    }
 }
