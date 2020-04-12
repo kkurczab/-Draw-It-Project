9470f623de5bfe93d2ae2f3a0f012ca206eac321
Karolz98 <44200387+Karolz98@users.noreply.github.com>
2020-04-12 23:07:52 +0200

    Rysowanko

--- src/GraGUI.java
+++ src/GraGUI.java
@@ -1,7 +1,10 @@
 import javax.swing.*;
+import javax.swing.border.BevelBorder;
+import javax.swing.event.ChangeEvent;
 import java.awt.*;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
+import java.awt.event.*;
+import java.awt.geom.Line2D;
+import java.awt.image.BufferedImage;
 
 public class GraGUI {
 
@@ -22,6 +25,11 @@ public class GraGUI {
     private JFormattedTextField formattedTextField5;
 
     public boolean czyKoniecGry; //0-nie, 1-tak
+    int currentX, currentY, oldX, oldY;
+    BufferedImage canvas;
+    private boolean clicked;
+    private int value =15;
+    //double value = 5;
 
     private Gra gra;
 
@@ -29,9 +37,80 @@ public class GraGUI {
         oknoGry.setPreferredSize(new Dimension(400, 600));
 
         oknoGry.setVisible(true);
+        canvas = new BufferedImage(400,600,BufferedImage.TYPE_INT_RGB);
+
+        planszaRysunku.addMouseMotionListener(new MouseMotionAdapter() {
+            @Override
+            public void mouseDragged(MouseEvent e) {
+
+                currentX = e.getX();
+                currentY = e.getY();
+                updateCanvas();
+                oldX = currentX;
+                oldY = currentY;
+            }
+        });
+
+
+        planszaRysunku.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent e) {
+                clicked = true;
+                oldX = e.getX();
+                oldY  = e.getY();
+                updateCanvas();
+                clicked = false;
+
+            }
+        });
     }
 
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("GraGUI");
+        frame.setContentPane(new GraGUI().oknoGry);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
 
     public Gra getGra(){return gra;}
     public boolean getCzyKoniecGry(){ return czyKoniecGry;}
+
+    private void createUIComponents() {
+
+            planszaRysunku = new JPanel(){
+
+                protected void paintComponent(Graphics g) {
+                    super.paintComponent(g);
+                    Graphics2D g2 = (Graphics2D) g;
+                    g2.drawImage(canvas, 0, 0, null);
+                }
+
+            };
+
+
+
+    }
+    public void updateCanvas() {
+        Graphics2D g2d = canvas.createGraphics();
+        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING,RenderingHints.VALUE_ANTIALIAS_ON);
+
+        g2d.setPaint(Color.red);
+
+        if (clicked==true)
+            g2d.fillOval(oldX - ((int) value / 2), oldY - ((int) value / 2), (int) value, (int) value);
+        else {
+            g2d.setStroke(new BasicStroke((float) value, BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND));
+            g2d.drawLine(oldX, oldY, currentX, currentY);
+            g2d.setStroke(new BasicStroke(1.0f));
+        }
+        planszaRysunku.repaint();
+    }
+
+
+
+/////
+
+
+    public void refresh(){}
 }
